f1eff1410ed047b3c486de593c7f17bb932fea5a
mklavin <mklavin@haverford.edu>
2023-10-29 19:25:22 -0400

    new repository

--- baselineremov.py
+++ baselineremov.py
@@ -9,7 +9,61 @@ import pybaselines.classification
 import pybaselines.optimizers
 import pybaselines.misc
 import numpy as np
-from preprocessing import normalize, smooth_spectra
+from preprocessing import remove_baseline, normalize, smooth_spectra, scale_rows_to_max
+from itertools import permutations
+
+def put_together_preprocess_search(data, conc, region:str):
+    """
+    :param data: dataframe of raw raman data
+    :param conc: concentrations of each spectra
+    :param region: region of the raman spectra
+    :return: preprocessed dataframe that gives the strongest correlation
+
+    search algorithm for the best preprocessing method
+    combining baseline removal, smoothing, normalization, and standardization
+    uses correlation analysis to evaluate
+    warning: takes a long time to run!
+    """
+    options = ['smoothing', 'normalizing', 'standardize', 'baseline removal'] # possible preprocessing
+    len_permutations = [3, 4] # first try all combinations of 3 of the preprocessing methods, then combos of 4
+    total_perm = []
+    for i in len_permutations:
+        all_permutations = list(permutations(options, i))
+        total_perm.append(all_permutations)
+    total_perm = [item for sublist in total_perm for item in sublist] # making a bigger list of permutations
+    results = []
+
+    for n in total_perm:
+        df = data
+        for i in n:
+            # if i == 'smoothing':              # uncover to check for smoothing, for some reason doesn't work with baseline stuff
+            #     df = smooth_spectra(df)
+            if i == 'normalizing':
+                df = normalize(df)
+            if i == 'standardize':
+                df = scale_rows_to_max(df, region)
+            if i == 'baseline removal':
+                df = baseline_search(df, conc)
+
+        df = pd.concat([df, conc], axis=1)
+        df = pd.DataFrame(np.corrcoef(df.T))
+        sorted_df = df.sort_values(by=1338, ascending=False)
+        sorted_df = sorted_df[[1338]].iloc[1:]
+        results.append(max(sorted_df[1338]))
+
+    max_index = results.index(max(results))
+
+    print(str(all_permutations[max_index]), max(results)) # the best model with the best correlation
+
+    for i in all_permutations[max_index]:
+        if i == 'normalizing':
+            df = normalize(df)
+        if i == 'standardize':
+            df = scale_rows_to_max(df, region)
+        if i == 'baseline removal':
+            df = baseline_search(df, conc)
+
+    return df
 
 def polynomial_search(df, conc):
     func = [pybaselines.polynomial.poly, pybaselines.polynomial.modpoly, pybaselines.polynomial.imodpoly,
@@ -21,7 +75,7 @@ def polynomial_search(df, conc):
     # polynomial fitting for baseline removal
     for j in func:
         polyorders = []
-        for x in range(1, 10):
+        for x in range(3, 10):
             baseline_removed = []
             for index, rowy in df.iterrows():
                 row = rowy.values.reshape(-1, 1)
@@ -49,13 +103,20 @@ def polynomial_search(df, conc):
         max_poly = polyorders.index(max(polyorders))
         results.append([j, max_poly, max(polyorders)])
 
-    # max_index = results.index(max(results))
+    max_index = max(range(len(results)), key=lambda i: results[i][2])
 
     print(max(results, key=lambda x: x[2]))
 
-    return None
+    return remove_baseline(df, func[max_index], results[max_index][1])
 
 def baseline_search(df, conc):
+    """
+    :param df: dataframe of all raman spectra
+    :param conc: concentrations of glutathione in each spectra
+    :return: return the dataframe with the baseline subtracted that gives the highest correlation
+    This function searches all of the pybaseline functions to find the one that gives the highest correlation
+    between the glutathione peak and concentration
+    """
     func = [pybaselines.whittaker.asls, pybaselines.whittaker.iasls, pybaselines.whittaker.airpls,
             pybaselines.whittaker.arpls, pybaselines.whittaker.drpls, pybaselines.whittaker.iarpls,
             pybaselines.whittaker.aspls, pybaselines.whittaker.psalsa, pybaselines.whittaker.derpsalsa,
@@ -67,7 +128,9 @@ def baseline_search(df, conc):
             pybaselines.spline.pspline_airpls, pybaselines.spline.pspline_arpls, pybaselines.spline.pspline_drpls,
             pybaselines.spline.pspline_iarpls, pybaselines.spline.pspline_aspls, pybaselines.spline.pspline_psalsa,
             pybaselines.spline.pspline_derpsalsa, pybaselines.smooth.noise_median, pybaselines.smooth.snip,
-            pybaselines.smooth.swima, pybaselines.smooth.ipsa, pybaselines.smooth.ria]
+            pybaselines.smooth.swima, pybaselines.smooth.ipsa, pybaselines.smooth.ria, pybaselines.classification.dietrich,
+            pybaselines.classification.golotvin, pybaselines.classification.std_distribution, pybaselines.classification.fastchrom, pybaselines.classification.fabc,
+            pybaselines.optimizers.optimize_extended_range, pybaselines.optimizers.adaptive_minmax, pybaselines.misc.beads]
     results = []
 
     # polynomial fitting for baseline removal
@@ -105,7 +168,7 @@ def baseline_search(df, conc):
 
     print(str(func[max_index]), max(results))
 
-    return None
+    return remove_baseline(df, func[max_index])
 
 
 
@@ -113,5 +176,8 @@ if __name__ == '__main__':
     df = pd.read_csv('data/data_610.csv')
     conc = pd.read_csv('data/data_610_concentrations_GSH.csv')
 
+    df = normalize(df)
+    df = scale_rows_to_max(df, '610')
 
-    polynomial_search(df, conc)
+
+    print(put_together_preprocess_search(df, conc, '610'))
--- preprocessing.py
+++ preprocessing.py
@@ -3,13 +3,24 @@ import pandas as pd
 import pybaselines.polynomial
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA, FastICA
-from scipy.signal import savgol_filter, argrelmin
 import numpy as np
 from scipy.signal import find_peaks
+from sklearn.preprocessing import StandardScaler
+from scipy.signal import savgol_filter, argrelmin
+
+"""
+ALL PREPROCESSING METHODS
+
+contains functions for standardizing spectra, normalizing, 
+baseline removal, PCA, spectral smoothing
+"""
 
 def standardize_byscalar(spectra, scalar):
-    # multiply all values by a scalar
-    # used to standardize spectra
+    """
+    :param spectra: row of dataframe
+    :param scalar: scalar to multiply all the spectra by
+    :return: row of databrame multiplied by a scalar
+    """
 
     spectra_scaled = spectra * scalar
     # uncover to visualize scaling
@@ -18,22 +29,12 @@ def standardize_byscalar(spectra, scalar):
     # plt.show()
     return spectra_scaled
 
-def normalize(spectra):
-    normalized = []
-
-    for index, rowy in spectra.iterrows():
-        row = rowy.values.reshape(-1, 1)
-        row = StandardScaler().fit_transform(row)
-        row = row.flatten()
-        row = row.reshape(1, -1)
-        normalized_df = pd.DataFrame(row, columns=rowy.index)
-        normalized.append(normalized_df)
-    normalized_spectra = pd.concat(normalized, axis=0, ignore_index=True)
-
-    return normalized_spectra
-
-
 def PCA1(data, n):
+    """
+    :param data: dataframe of raman spectra, mean zero std one
+    :param n: number of components for PCA analysis
+    :return: PCA dataframe
+    """
     columns_ = [i for i in range(n)]
     pca = PCA(n_components=n)
     pca = pca.fit(data)
@@ -42,31 +43,6 @@ def PCA1(data, n):
     pca_Df = pd.DataFrame(data=pca_components, columns=columns_)
     return pca_Df, ratio
 
-
-def remove_baseline(spectra):
-    # polynomial fitting for baseline removal
-
-    baseline_removed = []
-    i = 0
-    for index, rowy in spectra.iterrows():
-        row = rowy.values.reshape(-1, 1)
-        row = row.flatten()
-        row_polyfit = pybaselines.polynomial.imodpoly(row, poly_order=8)[0]
-        # plt.title(str(i))
-        # plt.plot(row)
-        # plt.plot(row_polyfit)
-        # plt.show()
-        row = row - row_polyfit
-        row = row.flatten()
-        row = row.reshape(1, -1)
-        normalized_df = pd.DataFrame(row, columns=rowy.index)
-        baseline_removed.append(normalized_df)
-        i += 1
-
-    baselined_spectra = pd.concat(baseline_removed, axis=0, ignore_index=True)
-
-    return baselined_spectra
-
 def fast_ICA(data, n):
     model = FastICA(n_components= n, whiten='unit-variance')
     columns_ = [i for i in range(n)]
@@ -74,7 +50,53 @@ def fast_ICA(data, n):
     data = pd.DataFrame(data=data, columns=columns_)
     return data
 
+def subtract_solventspec(data, solventspec):
+    """
+    :param data: dataframe of raman spectra
+    :param solventspec: spectra to subtract from the dataframe
+    :return: dataframe after subtraction
+    """
+    baseline_removed = []
+    for index, rowy in data.iterrows():
+        row = rowy.values.reshape(-1, 1)
+        row = row.flatten()
+        # plt.plot(row)
+        # plt.plot(solventspec)
+        # row = row - solventspec
+        # plt.show()
+        # print(row)
+        #row = row.reshape(1, -1)
+        normalized_df = pd.DataFrame(row, columns=rowy.index)
+        baseline_removed.append(normalized_df)
+
+    baselined_spectra = pd.concat(baseline_removed, axis=0, ignore_index=True)
+
+    return baselined_spectra
+
+def scale_rows_to_max(dataframe, region:str):
+    """
+    :param dataframe: dataframe of raman spectra
+    :param region: 580 or 610 spectral region, determines where to search for the water peak
+    :return: dataframe of raman spectra scaled to the water peak
+    """
+    scaled_dataframe = dataframe.copy()  # Make a copy of the input DataFrame to avoid modifying it
+
+    # Calculate the maximum value in each row
+    if region == '610':
+        max_values = dataframe.iloc[:, 1063:].max(axis=1)
+    if region == '580':
+        max_values = dataframe.iloc[:, 650:801].max(axis=1)
+
+    # Scale each row to have the same maximum value (target_max)
+    scaled_dataframe = scaled_dataframe.mul(20 / max_values, axis=0)
+
+    return scaled_dataframe
+
 def smooth_spectra(data):
+    """
+    :param data: dataframe of raman spectra
+    :return: smoothed dataframe
+    """
     # Parameters:
     w = 9  # window (number of points)
     p = 2  # polynomial order
@@ -101,91 +123,64 @@ def smooth_spectra(data):
 
     return baselined_spectra
 
-def subtract_solventspec(data, solventspec):
+def normalize(spectra):
+    """
+    :param spectra: dataframe of raman spectra
+    :return: normalized rows of dataframe
+    """
+    normalized = []
+
+    for index, rowy in spectra.iterrows():
+        row = rowy.values.reshape(-1, 1)
+        row = StandardScaler().fit_transform(row)
+        row = row.flatten()
+        row = row.reshape(1, -1)
+        normalized_df = pd.DataFrame(row, columns=rowy.index)
+        normalized.append(normalized_df)
+    normalized_spectra = pd.concat(normalized, axis=0, ignore_index=True)
+
+    return normalized_spectra
+
+def remove_baseline(spectra, baseline_func, order=None):
+    """
+    :param spectra: dataframe of raman spectra
+    :param baseline_func: pybaselines function to use
+    :return: dataframe of raman spectra with baselines subtracted
+    """
+    # polynomial fitting for baseline removal
+
     baseline_removed = []
-    for index, rowy in data.iterrows():
+    i = 0
+    for index, rowy in spectra.iterrows():
         row = rowy.values.reshape(-1, 1)
         row = row.flatten()
+        if order is not None:
+            row_polyfit = baseline_func(row, poly_order=order)[0]
+        else:
+            row_polyfit = baseline_func(row)[0]
+        # plt.title(str(i))
         # plt.plot(row)
-        # plt.plot(solventspec)
-        # row = row - solventspec
+        # plt.plot(row_polyfit)
         # plt.show()
-        # print(row)
-        #row = row.reshape(1, -1)
+        row = row - row_polyfit
+        row = row.flatten()
+        row = row.reshape(1, -1)
         normalized_df = pd.DataFrame(row, columns=rowy.index)
         baseline_removed.append(normalized_df)
+        i += 1
 
     baselined_spectra = pd.concat(baseline_removed, axis=0, ignore_index=True)
 
     return baselined_spectra
 
-def scale_rows_to_max(dataframe):
-    scaled_dataframe = dataframe.copy()  # Make a copy of the input DataFrame to avoid modifying it
-
-    # Calculate the maximum value in each row
-    max_values = dataframe.max(axis=1)
-
-    # Scale each row to have the same maximum value (target_max)
-    scaled_dataframe = scaled_dataframe.mul(20 / max_values, axis=0)
-
-    return scaled_dataframe
-
-def preprocess1(df):
-    # smooth, remove baseline, normalize, PCA var > 99.5%
-
-    df = smooth_spectra(df)
-    df = remove_baseline(df)
-    df = normalize(df)
-
-    # for i in range(len(df.iloc[0])):
-    #     plt.plot(df.iloc[i])
-    #     plt.show()
-
-    for i in range(40):
-        pca_Df, ratio = PCA1(df, i)
-        if sum(ratio) > 0.995:
-            print(sum(ratio),i)
-            break
-    return pca_Df
-
 
 if __name__ == '__main__':
-    df = pd.read_csv('data/data_610.csv')
+    df = pd.read_csv('data/data_580.csv')
     conc = pd.read_csv('data/data_580_concentrations_GSSG.csv')
     names = pd.read_csv('data/danielmimi_data_580_names.csv')
 
-    plt.plot(np.arange(0, len(df.iloc[3])), df.iloc[3])
-    plt.show()
-    exit()
-
-    df = smooth_spectra(df)
-    df = remove_baseline(df)
-    df = normalize(df)
-
-    total = pd.concat([df, conc], axis=1)
-
-    df = pd.DataFrame(np.corrcoef(total.T))
-    # Add an index column
-    df.reset_index(inplace=True)
-
-    # Sort column 'A' in ascending order while preserving the index
-    sorted_df = df.sort_values(by=1338, ascending=False)
-    sorted_df = sorted_df[['index', 1338]]
-
-    sorted_df.to_csv('data/corr_anal_580.csv', index=False)
-    exit()
-    # how well does model perform when bad spectra are dropped?
-    # bad_spec = [4,5,9,8,10,13,15,16,17,19,20,22,34,39,40,51,53,55,59,64,68,73,74,80,87,97]
-    # cutdf = df.drop(bad_spec)
-    # cutconc = conc.drop(bad_spec)
-
-    plt.plot(df.iloc[2])
-    plt.show()
-    exit()
 
 
-    df = preprocess5(df)
-    df.to_csv('data/prepro_methods/danielmimi_580_pre4_27com.csv', index=False)
 
 
 
